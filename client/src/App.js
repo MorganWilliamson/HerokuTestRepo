d607ca0e9cee34a48d633345497e416bf89ad28b
MorganWilliamson <mwilliamson7695@gmail.com>
2020-12-03 12:53:58 -0600

    url function

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+
+// NPM Run Start: Development
+// NPM Run Build: Production
+
+function url(path) {
+  return process.env.NODE_ENV === 'development' 
+  ? `http://localhost:5000${path}` 
+  : ''
+};
 
 function App() {
+  const [data, setData] = useState('')
+
+  useEffect(() => {
+    fetch(url('/api/data'))
+      .then((res) => res.json())
+      .then((theGoods) => setData(theGoods))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
+          { data }
           wowowowowowowowowowowowow
         </p>
       </header>
